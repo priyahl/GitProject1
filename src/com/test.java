7e602e113602714da47c50fbe3624536ee9d7fcd
priyahl <hlpriya125@gmail.com>
2023-08-14 12:02:49 -0700

    commit3

--- src/com/test.java
+++ src/com/test.java
@@ -5,6 +5,7 @@ public class test {
 	//main method
 	public static void main(String[] args) {
 		String hungry="yes";
+		//if block
 		if(hungry=="yes")
 		{
 			System.out.println("eat something");
